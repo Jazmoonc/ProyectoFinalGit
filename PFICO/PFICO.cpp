b4a98b91b1013cb53cd2ffa6bf47ea05607137f5
Jazmoonc <jazminmonse1231@outlook.es>
2022-11-30 17:36:09 -0600

    Se coloca otro if

--- PFICO/PFICO.cpp
+++ PFICO/PFICO.cpp
@@ -3,18 +3,27 @@
 void main(void)
 {
     char zona;
-    float sueldoBruto, sueldoNeto, porcentaje;
+    float sueldoBruto,
+    float sueldoNeto; 
+    float porcentaje;
 
     printf("Sueldo: ");
     scanf_s("%f", &sueldoBruto);
     //El problema planteado contiene 2 zonas A y B, a continuacion se pide la zona
     printf("Zona: ");
     scanf_s("%c", &zona);
-    //Se e´mpieza un estructura de control 
+
+    //Se empieza un estructura de control 
     if (zona == 'A')
     {
-        if (sueldoBruto <= 1850 || sueldoBruto < 3000)
+        if (sueldoBruto >= 1850 || sueldoBruto < 3000)
         {
+            porcentaje = .1;
+        }
+        else
+        {
+            if (sueldoBruto >=3000||sueldoBruto<5000)
+            { }
         }
     }
 }
