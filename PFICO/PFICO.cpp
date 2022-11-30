4b592f54cb79b88621d2f310d4a88bd4d6eb6196
Jazmoonc <jazminmonse1231@outlook.es>
2022-11-30 17:17:55 -0600

    Printf ZONA

--- PFICO/PFICO.cpp
+++ PFICO/PFICO.cpp
@@ -7,4 +7,7 @@ void main(void)
 
     printf("Sueldo: ");
     scanf_s("%f", &sueldoBruto);
+    //El problema planteado contiene 2 zonas A y B, a continuacion se pide la zona
+    printf("Zona: ");
+    scanf_s("%c, zona");
 }
